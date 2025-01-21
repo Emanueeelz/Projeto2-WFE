ca5f771e2b11500afc7c8036e33c16a0b1f04164
Emanueeelz <e.correia@escolar.ifrn.edu.br>
2025-01-20 22:02:48 -0300

    botão show de bola

--- projeto_2/src/app/_components/rodape/rodape.component.ts
+++ projeto_2/src/app/_components/rodape/rodape.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ContatoComponent } from '../contato/contato.component';
 
 @Component({
@@ -6,8 +6,28 @@ import { ContatoComponent } from '../contato/contato.component';
   standalone: true,
   imports: [ContatoComponent],
   templateUrl: './rodape.component.html',
-  styleUrl: './rodape.component.css'
+  styleUrls: ['./rodape.component.css']
 })
 export class RodapeComponent {
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const backToTopButton = document.getElementById('back-to-top');
+    const scrollPosition = window.pageYOffset;
+    const documentHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+
+    if (scrollPosition > 100 && scrollPosition < (documentHeight - windowHeight - 200)) {
+      backToTopButton?.classList.add('show');
+    } else {
+      backToTopButton?.classList.remove('show');
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
