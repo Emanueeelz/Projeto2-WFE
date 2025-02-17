c59b3695c595557a8ba6f2f3bbe49b18bb0ad4cf
Emanueeelz <e.correia@escolar.ifrn.edu.br>
2025-02-16 21:07:09 -0300

    deus na defesa e eu mesmo no ataque

--- projeto_2/src/app/app.component.ts
+++ projeto_2/src/app/app.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './_components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './_components/rodape/rodape.component';
 import { CartaoComponent } from './_components/cartao/cartao.component';
 import { CorpoSiteComponent } from "./_components/corpo-site/corpo-site.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CartaoComponent, CorpoSiteComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CartaoComponent, CorpoSiteComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'projeto_2';
+  currentRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+
+  isHomeRoute(): boolean {
+    return this.currentRoute === '/';
+  }
 }
