f1a4d22dbf044e544a0825c58ebc432ad9038a17
Raynara <raynara.gustavo@escolar.ifrn.edu.br>
2025-02-20 14:29:07 -0300

    background image corrigida

--- projeto_2/src/app/_components/formulario/formulario.component.ts
+++ projeto_2/src/app/_components/formulario/formulario.component.ts
@@ -1,26 +1,24 @@
 import { Component, inject } from '@angular/core';
-import { AuthService } from '../../services/auth.service';  // Importa o AuthService
+import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [FormsModule],  // Adiciona FormsModule para ngModel funcionar
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
-  nome: string = '';  // Variável para armazenar o nome
-  email: string = '';  // Variável para armazenar o e-mail
-  senha: string = '';  // Variável para armazenar a senha (não será usada agora)
+  nome: string = '';
+  email: string = '';
+  senha: string = '';
 
-  // Injetando o AuthService
   authService = inject(AuthService);
 
-  // Método de login chamado ao enviar o formulário
   login() {
     if (this.nome && this.email) {
-      this.authService.login(this.nome, this.email);  // Passa nome e e-mail
+      this.authService.login(this.nome, this.email);
     }
   }
 }
