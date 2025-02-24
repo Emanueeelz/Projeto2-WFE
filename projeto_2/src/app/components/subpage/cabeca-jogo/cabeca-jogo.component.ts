3cae42ba399564507961e654d63e137161b56444
Marcelo <henrique.cordeiro@escolar.ifrn.edu.br>
2025-02-24 18:56:55 -0300

    favoritos duplicados

--- projeto_2/src/app/components/subpage/cabeca-jogo/cabeca-jogo.component.ts
+++ projeto_2/src/app/components/subpage/cabeca-jogo/cabeca-jogo.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoritosService } from '../../../services/favoritos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabeca-jogo',
@@ -9,13 +10,52 @@ import { FavoritosService } from '../../../services/favoritos.service';
   templateUrl: './cabeca-jogo.component.html',
   styleUrls: ['./cabeca-jogo.component.css']
 })
-export class CabecaJogoComponent {
-  favoritosService = inject(FavoritosService);
+export class CabecaJogoComponent implements OnInit {
   favoritos: string[] = [];
 
-  constructor() {
+  constructor(
+    @Inject(FavoritosService) private favoritosService: FavoritosService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
     this.favoritosService.getFavoritosObservable().subscribe(favs => {
       this.favoritos = favs;
     });
+
+    this.favoritos = this.favoritosService.getFavoritos();
+  }
+
+  irParaPagina(jogo: string) {
+    const rotas: { [key: string]: string } = {
+      'Clash Royale': 'clash',
+      'Cyberpunk 2077': 'cyberpunk',
+      'Dark Souls 3': 'dark-souls',
+      'Red Dead Redemption 2': 'red-dead',
+      "JoJo's Bizarre Adventure: Heritage for the Future": 'jojo',
+      'Chrono Trigger': 'chrono-trigger',
+      'Counter-Strike: Global Offensive': 'csgo',
+      'Mineirinho Ultra Adventure': 'mineirinho',
+      'Blasphemous': 'blasphemous'
+    };
+
+    const rota = rotas[jogo] || 'home';
+
+    const modalElement = document.getElementById('favoritosModal');
+    if (modalElement) {
+      modalElement.classList.remove('show');
+      modalElement.setAttribute('aria-hidden', 'true');
+      modalElement.style.display = 'none';
+    }
+
+    const backdrop = document.querySelector('.modal-backdrop');
+    if (backdrop) {
+      backdrop.remove();
+    }
+
+    document.body.classList.remove('modal-open');
+    document.body.style.overflow = '';
+
+    this.router.navigate(['/', rota]);
   }
 }
