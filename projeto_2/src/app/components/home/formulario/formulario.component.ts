a93c4b8ae33651d02a1a5478b3b3ae1d62556629
pedro paulo <pedro.ferreira2@escolar.ifrn.edu.br>
2025-02-25 13:34:05 -0300

    forms fechando ao efetuar login

--- projeto_2/src/app/components/home/formulario/formulario.component.ts
+++ projeto_2/src/app/components/home/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 
@@ -9,12 +9,22 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
   nome: string = '';
   email: string = '';
   senha: string = '';
 
   authService = inject(AuthService);
+
+  @ViewChild('loginModal') loginModal!: ElementRef;
+
+  ngOnInit() {
+    this.authService.fecharPopup$.subscribe((fechar) => {
+      if (fechar && this.loginModal) {
+        (this.loginModal.nativeElement as any).click(); // Fecha o modal Bootstrap
+      }
+    });
+  }
 
   login() {
     if (this.nome && this.email) {
--- projeto_2/src/app/services/auth.service.ts
+++ projeto_2/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject} from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +8,19 @@ export class AuthService {
   private usuarioLogadoSubject = new BehaviorSubject<{ nome: string, email: string } | null>(null);
   usuarioLogado$ = this.usuarioLogadoSubject.asObservable();
 
+  private fecharPopupSubject = new BehaviorSubject<boolean>(false);
+  fecharPopup$ = this.fecharPopupSubject.asObservable();
+
   constructor() {}
 
   login(nome: string, email: string) {
     this.usuarioLogadoSubject.next({ nome, email });
+    this.fecharPopupSubject.next(true); // Emite o evento para fechar o modal
   }
 
   logout() {
     this.usuarioLogadoSubject.next(null);
   }
 }
+
+
