3a62f143d0cdca55fd3805f87afbaff9f47e4086
Emanueeelz <e.correia@escolar.ifrn.edu.br>
2025-02-25 23:18:53 -0300

    cuidado pra n dar conflito ae, pp

--- projeto_2/src/app/components/home/corpo-site/corpo-site.component.ts
+++ projeto_2/src/app/components/home/corpo-site/corpo-site.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from "../cabecalho/cabecalho.component";
 import { CartaoComponent } from "../cartao/cartao.component";
 import { RodapeComponent } from "../rodape/rodape.component";
-import { FormularioComponent } from "../formulario/formulario.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
